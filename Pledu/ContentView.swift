2608896f09ff4a7e0ebba728f0c55bd497e8ae0c
mnashrullah <muhammadnashrullah.it@gmail.com>
2020-08-04 22:24:52 +0700

    Initial Commit

---
+++ Pledu/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Pledu
+//
+//  Created by Muhammad Nashrullah on 04/08/20.
+//  Copyright © 2020 Kepiting Bakau. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
