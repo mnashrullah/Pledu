ca04ab32b0ba563a19de588c1ad1ea38bc932b4d
mnashrullah <muhammadnashrullah.it@gmail.com>
2020-08-15 23:40:42 +0700

    add plant, move to tab2

--- GoTanam/ContentView.swift
+++ GoTanam/ContentView.swift
@@ -19,7 +19,7 @@ struct ContentView: View {
             if show || UserDefaults.standard.bool(forKey: initialLaunchKey){
     
                 TabView (selection: $selected) {
-                        DiscoveryView().tabItem({
+                        DiscoveryView(tabSelected: $selected).tabItem({
                             Image(systemName: Constants.TabBarImageName.tabBar0)
                                 .font(.body)
                             Text("\(Constants.TabBarText.tabBar0)")
--- GoTanam/Views/DiscoveryView.swift
+++ GoTanam/Views/DiscoveryView.swift
@@ -12,6 +12,7 @@ struct DiscoveryView: View {
 //    @ObservedObject var mData = getDataDiscover()
     @State var categories = [String: [Discover]]()
     @State var dataPlant = [Discover]()
+    @Binding var tabSelected : Int
     
     
     @State var showingSetting = false
@@ -82,7 +83,7 @@ struct DiscoveryView: View {
                         if self.searchText == ""{
                             ForEach(self.categories.keys.sorted(), id:\.self) {
                                 item in
-                                   CategoryRow(categoryName: item, items: self.categories[item]!)
+                                CategoryRow(categoryName: item, items: self.categories[item]!, tabSelected: self.$tabSelected)
                             }
 //                            .listRowInsets(EdgeInsets())
 //                            .resignKeyboardOnDragGesture()
@@ -90,7 +91,7 @@ struct DiscoveryView: View {
                         }else{
                               ForEach(self.dataPlant.filter{$0.name.lowercased().contains(searchText.lowercased()) || searchText == ""}, id:\.self) {searchText in
                             //                        NavigationLink(destination: ShowcontentView(isNavigationBarHidden: self.$isNavigationBarHidden, landmark: searchText)) {
-                                cardPlant(item: searchText)
+                                cardPlant(item: searchText, tabSelected: self.$tabSelected)
                             //
                                                     }
                             //                    }
@@ -154,6 +155,7 @@ extension View {
 struct CategoryRow: View{
     var categoryName: String
     var items: [Discover]
+    @Binding var tabSelected: Int
     
     var body: some View{
 
@@ -171,7 +173,7 @@ struct CategoryRow: View{
             ScrollView(.horizontal, showsIndicators: false) {
                 HStack(spacing: 20){
                     ForEach(0..<self.items.count, id: \.self){item in
-                        cardPlant(item: self.items[item])
+                        cardPlant(item: self.items[item], tabSelected: self.$tabSelected)
                         }.frame(height: 185)
                 }.padding(.leading, 10)
             }
@@ -185,7 +187,8 @@ struct cardPlant: View{
     @State var item : Discover
     @Environment(\.imageCache) var cache: ImageCache
     @State var show = false
-    
+    @Binding var tabSelected: Int
+    @Environment(\.presentationMode) var presentationMode
     var body: some View{
         
         VStack(alignment: .leading, spacing: 5){
@@ -210,144 +213,148 @@ struct cardPlant: View{
         .cornerRadius(9)
         .shadow(color: Color.black.opacity(0.10), radius: 2, x: 2, y: 2)
         .sheet(isPresented: $show){
-
             VStack{
-                VStack{
-                    closeModalButton()
-                    
-                    GeometryReader{ geo in
-                        ScrollView(showsIndicators: false){
-                            AsyncImage(url: URL(string: self.item.img)!, cache: self.cache, placeholder: ShimmerView().frame(width: 120, height: 120), configuration: { $0.resizable() })
-                                .aspectRatio(1, contentMode: .fill)
-                                .frame(width:UIScreen.main.bounds.width, height: 300)
-                            
-                            VStack(alignment: .leading, spacing: 10){
-                                HStack{
-                                    VStack(alignment: .leading){
-//                                        NavigationLink(destination:_){
-//
-                                            Text("Tutorial")
-                                                .fontWeight(.semibold)
-                                                .foregroundColor(Color.white)
-                                                .font(.subheadline)
-                                                .padding(.top, 0)
-                                                .padding()
-//                                        }
-                                        HStack{
-                                            Text("Cara menanam " + String(self.item.category.capitalized) + " " + String(self.item.name.capitalized)).fontWeight(.heavy).foregroundColor(Color.white).font(.title)
-                                        }
-                                            .padding(.top, -30)
-                                            .padding()
+                            VStack{
+            //                    closeModalButton()
+                                
+                                GeometryReader{ geo in
+                                    ScrollView(showsIndicators: false){
+                                        AsyncImage(url: URL(string: self.item.img)!, cache: self.cache, placeholder: ShimmerView().frame(width: 120, height: 120), configuration: { $0.resizable() })
+                                            .aspectRatio(1, contentMode: .fill)
+                                            .frame(width:UIScreen.main.bounds.width, height: 300)
                                         
+                                        VStack(alignment: .leading, spacing: 10){
+                                            HStack{
+                                                VStack(alignment: .leading){
+            //                                        NavigationLink(destination:_){
+            //
+                                                        Text("Tutorial")
+                                                            .fontWeight(.semibold)
+                                                            .foregroundColor(Color.white)
+                                                            .font(.subheadline)
+                                                            .padding(.top, 0)
+                                                            .padding()
+            //                                        }
+                                                    HStack{
+                                                        Text("Cara menanam " + String(self.item.category.capitalized) + " " + String(self.item.name.capitalized)).fontWeight(.heavy).foregroundColor(Color.white).font(.title)
+                                                    }
+                                                        .padding(.top, -30)
+                                                        .padding()
+                                                    
+                                                        
+                                                }
+                                                Spacer()
+                                            }
+                                            .background(AsyncImage(url: URL(string: self.item.img)!, cache: self.cache, placeholder: ShimmerView().frame(width:UIScreen.main.bounds.width, height: 120), configuration: { $0.resizable() })
+
+                                                .frame(width:UIScreen.main.bounds.width, height: 100.0)
+                                                .blur(radius: 40, opaque: true)
+                                            )
+                                            .padding(.top, -80)
                                             
+                                            VStack(){
+                                                Text(self.item.name.capitalized).fontWeight(.semibold)
+                                                Text("Durasi tumbuh " + String(self.item.duration) + " Hari")
+                                            }
+                                            .frame(width:UIScreen.main.bounds.width)
+                                        }
+                                        
+                                        VStack(alignment: .leading, spacing: 10){
+                                            
+                                            Text(self.item.description.capitalized).foregroundColor(.gray).font(Font.system(size:14, design: .default))
+                                            
+                                            Text("Alat dan Bahan").fontWeight(.semibold)
+                                            Text(self.item.tools).foregroundColor(.gray).font(Font.system(size:14, design: .default))
+                                            
+                                        }.padding()
+                                        
+                                    }.background(Color("clearColor"))
+
+
+                                }.background(Color.white)
+                                .padding(.top, -75)
+                                HStack{
+                //                    Button(action: {
+                //
+                //                    }) {
+                //
+                //                        Image("Save").renderingMode(.original)
+                //                    }
+                                    Button(action: {
+                                        self.addToMyPlant(item: self.item)
+                                        self.show = false
+                                        
+
+                                    }) {
+                                        HStack(spacing: 6){
+
+                                            Text("Tambah ke Tanamanku")
+                                                .fontWeight(.regular)
+                                                .foregroundColor(.white)
+                                                .padding(.leading, 0)
+                                            
+                                            Text("Pilih")
+                                                .foregroundColor(.blue)
+                                                .fontWeight(.semibold)
+                                                .background(Color.white
+                                                    .cornerRadius(10)
+                                                    .frame(width: 50)
+                                                )
+                                                .offset(x: UIScreen.main.bounds.width/13)
+
+                                        }.foregroundColor(.red)
+                                        .padding()
                                     }
-                                    Spacer()
-                                }
-                                .background(AsyncImage(url: URL(string: self.item.img)!, cache: self.cache, placeholder: ShimmerView().frame(width:UIScreen.main.bounds.width, height: 120), configuration: { $0.resizable() })
-
-                                    .frame(width:UIScreen.main.bounds.width, height: 100.0)
-                                    .blur(radius: 40, opaque: true)
-                                )
-                                .padding(.top, -80)
-                                
-                                VStack(){
-                                    Text(self.item.name.capitalized).fontWeight(.semibold)
-                                    Text("Durasi tumbuh " + String(self.item.duration) + " Hari")
-                                }
-                                .frame(width:UIScreen.main.bounds.width)
-                            }
-                            
-                            VStack(alignment: .leading, spacing: 10){
-                                
-                                Text(self.item.description.capitalized).foregroundColor(.gray).font(Font.system(size:14, design: .default))
-                                
-                                Text("Alat dan Bahan").fontWeight(.semibold)
-                                Text(self.item.tools).foregroundColor(.gray).font(Font.system(size:14, design: .default))
-                                
-                            }.padding()
-                            
-                        }.background(Color("clearColor"))
-
-
-                    }.background(Color.white)
-                    .padding(.top, -75)
-                    HStack{
-    //                    Button(action: {
-    //
-    //                    }) {
-    //
-    //                        Image("Save").renderingMode(.original)
-    //                    }
-                        Button(action: {
-                            self.addToMyPlant(item: self.item)
-
-                        }) {
-                            HStack(spacing: 6){
-
-                                Text("Tambah ke Tanamanku")
-                                    .fontWeight(.regular)
-                                    .foregroundColor(.white)
-                                    .padding(.leading, 0)
-                                
-                                Text("Pilih")
-                                    .foregroundColor(.blue)
-                                    .fontWeight(.semibold)
-                                    .background(Color.white
+                                    .background(Color("offColor")
+                                        .opacity(0.3)
+                                        .frame(width:UIScreen.main.bounds.width-40, height: 40.0)
                                         .cornerRadius(10)
-                                        .frame(width: 50)
                                     )
-                                    .offset(x: UIScreen.main.bounds.width/13)
+                                }
+                            }
+                        }.padding()
+                            .frame(maxWidth: .infinity, maxHeight: .infinity).background(Color("clearColor"))
 
-                            }.foregroundColor(.red)
-                            .padding()
-                        }
-                        .background(Color("offColor")
-                            .opacity(0.3)
-                            .frame(width:UIScreen.main.bounds.width-40, height: 40.0)
-                            .cornerRadius(10)
-                        )
-                    }
-                }
-            }.padding()
-                .frame(maxWidth: .infinity, maxHeight: .infinity).background(Color("clearColor"))
-     
+            
         }
     }
     func addToMyPlant(item: Discover){
-//        print("addToMyPlant ",item)
-        let url = Constants.Api.addMyPlant
-        let parameters = [
-            "idUser": UserDefaults.standard.integer(forKey: Constants.dataUserDefault.idUser),
-            "idPlant": self.item.idPlant
-        ]
-//        print("addToMyPlant parameter ", parameters)
+       //        print("addToMyPlant ",item)
+               let url = Constants.Api.addMyPlant
+               let parameters = [
+                   "idUser": UserDefaults.standard.integer(forKey: Constants.dataUserDefault.idUser),
+                   "idPlant": self.item.idPlant
+               ]
+       //        print("addToMyPlant parameter ", parameters)
 
-        var urlRequest = URLRequest(url: URL(string: url)!)
-        urlRequest.httpMethod = "POST"
-        urlRequest.addValue("application/json", forHTTPHeaderField: "Content-Type")
-        urlRequest.httpBody = try? JSONEncoder().encode(parameters)
+               var urlRequest = URLRequest(url: URL(string: url)!)
+               urlRequest.httpMethod = "POST"
+               urlRequest.addValue("application/json", forHTTPHeaderField: "Content-Type")
+               urlRequest.httpBody = try? JSONEncoder().encode(parameters)
 
-        URLSession.shared.dataTask(with: urlRequest)
-        {(data,response,err) in
-            if err != nil{
-                print((err?.localizedDescription)!)
-                return
-            }
-//            print("addToMyPlant", response)
-//            print("addToMyPlant", data)
-//            print("addToMyPlant", err)
-//            let json: [MyProgress] = try! JSONDecoder().decode([MyProgress].self, from: data!)
-//            DispatchQueue.main.async {
-//                print(json)
-//                print("berhasil simpan data")
-                
-//                self.dataProgress = json
-//                print("phaseview json ",self.dataProgress)
-                
-//            }
-            print("Fetch failed: \(err?.localizedDescription ?? "Unknown error")")
-        }.resume()
-    }
+               URLSession.shared.dataTask(with: urlRequest)
+               {(data,response,err) in
+                   if err != nil{
+                       print((err?.localizedDescription)!)
+                       return
+                   }
+                   self.tabSelected = 1
+       //            print("addToMyPlant", response)
+       //            print("addToMyPlant", data)
+       //            print("addToMyPlant", err)
+       //            let json: [MyProgress] = try! JSONDecoder().decode([MyProgress].self, from: data!)
+       //            DispatchQueue.main.async {
+       //                print(json)
+       //                print("berhasil simpan data")
+                       
+       //                self.dataProgress = json
+       //                print("phaseview json ",self.dataProgress)
+                       
+       //            }
+                   print("Fetch failed: \(err?.localizedDescription ?? "Unknown error")")
+               }.resume()
+           }
+   
 }
 
 struct closeModalButton: View {
